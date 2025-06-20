f950c15b4237e9abe07e385573ad7356ac298ca4
Mikaell Victor <mikaell.victor@hotmail.com>
2025-06-20 19:26:22 +0000

    Tabuada

---
+++ Loop_aninhado_FOR.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main (){
+    for (int i = 0; i <= 10; i++){
+        for (int j = 0; j <= 10; j++){
+        printf("Resultado %d \n", i * j);
+        }
+    printf("\n");
+    }
+    return 0;
+}
